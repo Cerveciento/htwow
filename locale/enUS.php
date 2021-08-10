6b881225364a17d7250e5180cf45ea8dace943be
Cerveciento <un.pone@gmail.com>
2021-08-10 13:31:05 +0200

    Locales esES, esMX y enUS

---
+++ locale/enUS.php
@@ -0,0 +1,45 @@
+<?php
+// App
+$locale[TXT_VERSION_WOW]  = "TrinityCore 3.3.5 (WotLK)";
+$locale[TXT_ID]           = "ID";
+$locale[TXT_MULTI_ID]     = "Multiple identifiers";
+$locale[TXT_VOLVER]       = "Back";
+$locale[TXT_ENVIAR]       = "Submit";
+$locale[TXT_SIGUIENTE]    = "Next";
+$locale[TXT_ANTERIOR]     = "Previous";
+$locale[TXT_NO_SIGUIENTE] = "There is no next";
+$locale[TXT_NO_ANTERIOR]  = "There is no previous";
+$locale[TXT_TOTALES]      = "TOTALS";
+// Substitutions
+$locale[TXT_NOMBRE]       = "name";
+$locale[TXT_RAZA]         = "race";
+$locale[TXT_CLASE]        = "class";
+// seeker
+$locale[TXT_LUGAR]        = "Place";
+$locale[TXT_POI]          = "Point of Interest";
+$locale[TXT_CRIATURA]     = "Creature";
+$locale[TXT_OBJETO]       = "Object";
+$locale[TXT_ITEM]         = "Item";
+$locale[TXT_BUSCAR]       = "Search";
+
+// Quests
+$locale[TXT_MIS_MISION]      = "Quest";
+$locale[TXT_MIS_MISIONES]    = "Quests";
+$locale[TXT_MIS_TITULO]      = "Title";
+$locale[TXT_MIS_DESCRIPCION] = "Description";
+$locale[TXT_MIS_OBJETIVOS]   = "Objectives";
+$locale[TXT_MIS_OBJETIVO_1]  = "Objective 1";
+$locale[TXT_MIS_OBJETIVO_2]  = "Objective 2";
+$locale[TXT_MIS_OBJETIVO_3]  = "Objective 3";
+$locale[TXT_MIS_OBJETIVO_4]  = "Objective 4";
+$locale[TXT_MIS_PROGRESO]    = "Progress";
+$locale[TXT_MIS_TERMINACION] = "Completion";
+$locale[TXT_MIS_EMPIEZA]     = "Start";
+$locale[TXT_MIS_TERMINA]     = "End";
+$locale[TXT_MIS_SERIE]       = "Series";
+$locale[TXT_MIS_REQUIERE]    = "Require";
+$locale[TXT_MIS_ABRE]        = "Open";
+$locale[TXT_MIS_OBJETIVO]    = "Objective";
+$locale[TXT_MIS_MATADO]      = "Slain";
+$locale[TXT_MIS_RECOMPENSA]  = "Reward";
+$locale[TXT_MIS_ELEGIR_RECOMPENSA] = "You will be able to choose one of these rewards:";
